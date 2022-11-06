29c8b9ff7592ce5bb47f07e71750c2b4d7a00eda
Alsu Kurmakaeva <kurmakaeva548@gmail.com>
2022-11-06 12:47:16 +0300

    fix file Data_Preporation

--- Data_Preporation.py
+++ Data_Preporation.py
@@ -4,7 +4,11 @@ import pandas as pd
 import os
 import pickle
 
-from dateutil.relativedelta import relativedelta
+# Create directoty:
+# data_result/result_export; data_result/result_import;
+# data_new
+
+# Also if you have new data, don't forget to delete it from the folder after processing
 
 
 def drop_col(data):
@@ -54,39 +58,89 @@ def period_to_datetime(data):
     data2.period = list_period
     return data2
 
+def remane_tnved(data):
+    data2 = data.copy()
+    data2 = data2.rename(columns={'tnved': '10-lvl-tnved'})
+    return data2
+
 def main():
     print('I am here!')
-    # read data from disk
-    dirpath = './data'
+
+    dirpath = './data_new'
     file_list = os.listdir(dirpath)
-    df = pd.DataFrame()
-    for filename in os.listdir(dirpath):
-        filepath = os.path.join(dirpath, filename, 'DATTSVT.csv')
-        data_new = pd.read_csv(filepath, sep='\t', low_memory=False)
-        df = pd.concat([df, data_new])
-    df = df.reset_index(drop=True)
+    print('There are new files!')
+    # check, if there is new data
+    if len(os.listdir(dirpath)) != 0:
+        df = pd.DataFrame()
+        for filename in os.listdir(dirpath):
+            filepath = os.path.join(dirpath, filename, 'DATTSVT.csv')
+            data_new = pd.read_csv(filepath, sep='\t', low_memory=False)
+            df = pd.concat([df, data_new])
+        df = df.reset_index(drop=True)
 
-    # transform_data
-    data = drop_col(df)
-    data = change_type(data)
-    data = level_create(data)
-    data = drop_outliers(data)
-    data = clean_tnved(data)
-    data = period_to_datetime(data)
+        data = drop_col(df)
+        data = change_type(data)
+        data = level_create(data)
+        data = drop_outliers(data)
+        data = clean_tnved(data)
+        data = period_to_datetime(data)
+        data = remane_tnved(data)
 
-    print(data.columns)
-    # Separate Export / import dataset
-    data_ex = data[data['napr'] == 'ЭК']
-    data_im = data[data['napr'] == 'ИМ']
+        # Separate Export / import dataset
+        data_ex = data[data['napr'] == 'ЭК']
+        data_im = data[data['napr'] == 'ИМ']
 
-    with open('./data_export.pickle', 'wb') as f:
-        pickle.dump(data_ex, f)
-        f.close()
+        with open(r"./data_result/result_export/data_export.pickle", "rb") as input_file:
+            df_old_exp = pickle.load(input_file)
+        with open(r"./data_result/result_import/data_import.pickle", "rb") as input_file:
+            df_old_imp = pickle.load(input_file)
+        data_ex = pd.concat([data_ex, df_old_exp])
+        data_ex = df.reset_index(drop=True)
+        data_im = pd.concat([data_im, df_old_imp])
+        data_im = df.reset_index(drop=True)
 
-    with open('./data_import.pickle', 'wb') as f:
-        pickle.dump(data_im, f)
-        f.close()
+        with open('./data_result/result_export/data_export.pickle', 'wb') as f:
+            pickle.dump(data_ex, f)
+            f.close()
+        with open('./data_result/result_import/data_import.pickle', 'wb') as f:
+            pickle.dump(data_im, f)
+            f.close()
 
-    print('Done!')
+        print('Concatenate new and all data!')
+
+    else:
+        dirpath = './data'
+        file_list = os.listdir(dirpath)
+        df = pd.DataFrame()
+        for filename in os.listdir(dirpath):
+            filepath = os.path.join(dirpath, filename, 'DATTSVT.csv')
+            data_new = pd.read_csv(filepath, sep='\t', low_memory=False)
+            df = pd.concat([df, data_new])
+        df = df.reset_index(drop=True)
+
+        # transform_data
+        data = drop_col(df)
+        data = change_type(data)
+        data = level_create(data)
+        data = drop_outliers(data)
+        data = clean_tnved(data)
+        data = period_to_datetime(data)
+        data = remane_tnved(data)
+
+        # Separate Export / import dataset
+        data_ex = data[data['napr'] == 'ЭК']
+        data_im = data[data['napr'] == 'ИМ']
+
+        with open('./data_result/result_export/data_export.pickle', 'wb') as f:
+            pickle.dump(data_ex, f)
+            f.close()
+
+        with open('./data_result/result_import/data_import.pickle', 'wb') as f:
+            pickle.dump(data_im, f)
+            f.close()
+
+        print('Done new data!')
+
 if __name__ == '__main__':
     main()
+
