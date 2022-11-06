ec4cce2e57a7e4134215d896f14728c8b89cab72
ThemedMayhem <83472088+ThemedMayhem@users.noreply.github.com>
2022-11-06 16:43:44 +0300

    Add files via upload

--- script_of_creating_csv.py
+++ script_of_creating_csv.py
@@ -8,19 +8,6 @@ with open(r"data_export.pickle", "rb") as input_file:
 with open(r"data_import.pickle", "rb") as input_file:
     data_new_im = pickle.load(input_file)
     
-with open('test.txt', encoding='utf-8') as f:
-    line = f.readline()
-    
-
-code = line.split(']')[0][1:].replace("'","").split(',')
-parse_params = line.split(']')[1][1:].split(',')
-code_lvl = int(parse_params[0])
-region = parse_params[1].replace("'","")
-country = parse_params[2].replace("'","")
-per_start = parse_params[3].replace("'","")
-per_end = parse_params[4].replace("'","")
-
-
 def get_request_by_region(data_new_im, data_new_ex,code='all', code_lvl=10, region='all', country='all', per_start='01-2018', per_end='12-2022'):
     per_start = datetime.strptime(per_start,'%m-%Y') - relativedelta(days = 15)
     per_end = datetime.strptime(per_end,'%m-%Y') + relativedelta(days = 15)
@@ -64,11 +51,33 @@ def get_request_by_region(data_new_im, data_new_ex,code='all', code_lvl=10, regi
                 if region != 'all' and country != 'all': 
                     list_of_napr[napr+'_'+c+'_'+region+'_'+country] = (data_new[(data_new['Region'] == region) & (data_new['nastranapr'] == country) & (data_new['period'] > per_start) & (data_new['period'] < per_end) & (data_new[tnved].astype(str) == c)].groupby(["nastranapr"]).Stoim.sum().reset_index().sort_values('Stoim',ascending=False).set_index("nastranapr"))
     return  list_of_napr
-
-
-df_top_n = get_request_by_region(data_new_im, data_new_ex, code=code, code_lvl=code_lvl, region=region, country = country, per_start=per_start, per_end=per_end)
-
-for key in (df_top_n):
-    df_top_n[key].to_csv(key)
+ 
     
+    
+    
+while True:
+    previous_line = ''
+    with open('params.txt', encoding='utf-8') as f:
+        line = f.readline()
+    
+    
+    if line!=previous_line:
+    
+        code = line.split(']')[0][1:].replace("'","").split(',')
+        parse_params = line.split(']')[1][1:].split(',')
+        code_lvl = int(parse_params[0])
+        region = parse_params[1].replace("'","")
+        country = parse_params[2].replace("'","")
+        per_start = parse_params[3].replace("'","")
+        per_end = parse_params[4].replace("'","")
+        
+        
+        
+        df_top_n = get_request_by_region(data_new_im, data_new_ex, code=code, code_lvl=code_lvl, region=region, country = country, per_start=per_start, per_end=per_end)
+        
+        for key in (df_top_n):
+            df_top_n[key].to_csv(key)
+            
+        previous_line = line
+        
     
